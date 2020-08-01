9ead767bd1055b87ddfc2b0723ed501ccec60cea
Steve Carmel <steve2christ@users.noreply.github.com>
2020-08-01 23:15:01 +0200

    wip

--- lib/pages/home/history_page.dart
+++ lib/pages/home/history_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:sba_web/pages/menu/drawer_menu.dart';
 
-import '../menu/drawer_menu.dart';
+import 'buch.dart';
 
 class HistoryPage extends StatefulWidget {
   String title;
@@ -12,6 +13,7 @@ class HistoryPage extends StatefulWidget {
 }
 
 class _HistoryPageState extends State<HistoryPage> {
+  List<Buch> _diebuecher = buecher;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,16 +38,46 @@ class _HistoryPageState extends State<HistoryPage> {
         ],
         //actionsIconTheme: IconThemeData(color: Colors.black, size: 30.0),
       ),
-
       body: SafeArea(
         child: Container(
-          color: Colors.green,
-          child: Center(
-            child: Text('Ici c\'est la page de l\'historique'),
+          color: Colors.white,
+          child: ListView(
+            children: _diebuecher.map(_buildOneBuch).toList(),
           ),
         ),
       ),
       drawer: Drawer(child: sbaDrawer),
     );
+  }
+
+  Widget _buildOneBuch(Buch dasBuch) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          top: BorderSide(width: 0.4, color: Colors.grey),
+          bottom: BorderSide(width: 0.4, color: Colors.grey),
+          right: BorderSide(width: 0.4, color: Colors.grey),
+          left: BorderSide(width: 0.4, color: Colors.grey),
+        ),
+        borderRadius: BorderRadius.circular(13.0),
+      ),
+      child: ListTile(
+        title: Text.rich(
+          TextSpan(
+            text: dasBuch.buchTitel,
+            style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+          ),
+        ),
+        subtitle: Text(dasBuch.buchAuthor),
+        trailing: Wrap(
+          spacing: 12, // space between two icons
+          children: <Widget>[
+            Text("12.06.2020"), // icon-1
+        Icon(Icons.arrow_forward_ios), // icon-2
+        ],
+      ),
+    ),
+    );
+
   }
 }
