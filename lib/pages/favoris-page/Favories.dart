4200683e62b4ff29f39ae941ca7d547ab36bdf99
Steve Carmel <steve2christ@users.noreply.github.com>
2020-08-06 22:11:44 +0200

    wip

---
+++ lib/pages/favoris-page/Favories.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:sba_web/pages/favoris-page/FavoriesBody.dart';
+import 'items.dart';
+
+class Favories extends StatelessWidget {
+
+  Future<List<Items>> fetchItems (BuildContext context) async{
+  final jsonstring = await DefaultAssetBundle.of(context).loadString('assets/Items.json');
+  return itemsFromJson(jsonstring);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Scaffold is a layout for the major Material Components.
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          tooltip: 'Back',
+          onPressed: null,//() => Navigator.of(context).pop(),
+        ),
+        title: Text('Favorite'),
+        centerTitle: true,
+      ),
+      // body is the majority of the screen.
+      body: FavoriesBody(),
+    );
+  }
+}
