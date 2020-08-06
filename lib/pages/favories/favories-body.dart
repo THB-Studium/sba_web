795c8fa2cb12908fccac75f1d86999d4b8c7ef24
Steve Carmel <steve2christ@users.noreply.github.com>
2020-08-06 22:28:51 +0200

    wip

--- lib/pages/favories/favories-body.dart
+++ lib/pages/favories/favories-body.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:sba_web/models/buch-for-histories.dart';
 
 
-class FavoriesBody extends StatelessWidget {
+class FavoriesBody extends StatefulWidget {
+  @override
+  _FavoriesBodyState createState() => _FavoriesBodyState();
+}
+
+class _FavoriesBodyState extends State<FavoriesBody> {
+  List<Buch> favoriesList = buecher;
+
   @override
   Widget build(BuildContext context) {
-    // Scaffold is a layout for the major Material Components.
     return Scaffold(
-      // body is the majority of the screen.
       body: Center(
         child: Text('This is the favory page :) <3 :D'),
       )
