07607d317bedfe22b3a86ab6651f4e219ce24d92
Steve Carmel <steve2christ@users.noreply.github.com>
2020-08-06 12:26:39 +0200

    somme correction in the Bookdetails view

--- lib/pages/search-book/Bookdetails/details-page.dart
+++ lib/pages/search-book/Bookdetails/details-page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sba_web/models/buch-for-histories.dart';
 import 'package:sba_web/pages/components/constants.dart';
-import 'package:sba_web/pages/components/menu/drawer_menu.dart';
 import 'package:sba_web/pages/favories/favories-page.dart';
 
 import '../search-page.dart';
@@ -19,8 +18,8 @@ class _BuchDetailPageState extends State<BuchDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(child: sbaDrawer(context)),
       appBar: AppBar(
+        leading: zurrueckButton(context),
         title: Text(widget.book.buchTitel),
         actions: <Widget>[
           actionButton(FavoritenPage(), Icons.star_half, context),
