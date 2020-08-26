ab683a685c99f364b15ac158e1eb9801411bd902
Steve Carmel <65890012+steve2christ@users.noreply.github.com>
2020-08-26 03:33:26 +0200

    Issue 42 implement the ber uns view in the menu view (#51)
    * fertig
    
    fertig
    
    * Approuved
    
    Co-authored-by: Astrid237 <65953173+Astrid237@users.noreply.github.com>
    Co-authored-by: Steve Carmel <steve2christ@users.noreply.github.com>

--- lib/pages/components/menu/drawer_menu.dart
+++ lib/pages/components/menu/drawer_menu.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sba_web/pages/components/constants.dart';
 import 'package:sba_web/pages/login/login-page.dart';
+import 'package:sba_web/pages/search-book/search-widgets.dart';
 
 Widget sbaDrawer(BuildContext context) {
   return Container(
@@ -48,6 +49,9 @@ Widget sbaDrawer(BuildContext context) {
         ListTile(
           leading: Icon(Icons.person),
           title: Text('Über uns'),
+          onTap: () {
+            openMyDialog(context);
+          }
         ),
         ListTile(
           leading: Icon(Icons.do_not_disturb_on),
@@ -60,6 +64,183 @@ Widget sbaDrawer(BuildContext context) {
         ),
       ],
     ),
+  );
+}
+
+openMyDialog(context) {
+  showDialog(context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context)
+  {
+    return AlertDialog(
+      backgroundColor: Color(0xffE6E6FA),
+        title: Text("Über uns"),
+        contentPadding: EdgeInsets.all(6),
+        content: SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+            ),
+            child: IntrinsicHeight(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    // A fixed-height child.
+                    //color: Color(0xffF0FFFF),
+                    height: 40,
+                    alignment: Alignment.center,
+                    child: const Text('Unser Team'),
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * 0.70,
+                      width: MediaQuery.of(context).size.width * 0.90,
+                      child: GridView.count(
+                        childAspectRatio: 0.6,
+                        padding: const EdgeInsets.only(top: 10),
+                        crossAxisSpacing: 6,
+                        mainAxisSpacing: 6,
+                        crossAxisCount: 2,
+                        children: <Widget>[
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              image: DecorationImage(
+                                fit: BoxFit.fitWidth,
+                                image: ExactAssetImage("assets/about_images/steve.jpeg"),
+                              ),
+                              color: Colors.indigo,
+                            ),
+                            padding: const EdgeInsets.all(8),
+                            child: Stack(
+                              children: <Widget>[
+                                Align(
+                                  alignment: Alignment.topCenter,
+                                  child: Text("A. Steve Ngalamo",
+                                    style: TextStyle(color: Colors.white),),
+                                ),
+                                Align(
+                                  alignment: Alignment.bottomCenter,
+                                  child: Text("Front- und Backend",
+                                    style: TextStyle(color: Colors.white),),
+                                ),
+                              ],
+                            ),
+
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              image: DecorationImage(
+                                image: ExactAssetImage("assets/about_images/flora.PNG"),
+                              ),
+                              color: Colors.indigo,
+                            ),
+                            padding: const EdgeInsets.all(8),
+                            child: Stack(
+                              children: <Widget>[
+                                Align(
+                                  alignment: Alignment.topCenter,
+                                  child: Text("Astride F. Goufack",
+                                    style: TextStyle(color: Colors.white),),
+                                ),
+
+                                Align(
+                                  alignment: Alignment.bottomCenter,
+                                  child: Text("Frontend",
+                                    style: TextStyle(color: Colors.white),),
+                                ),
+                              ],
+                            ),
+
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              image: DecorationImage(
+                                fit: BoxFit.fitWidth,
+                                image: ExactAssetImage("assets/about_images/junior_2.jpeg"),
+                              ),
+                              color: Colors.indigo,
+                            ),
+                            padding: const EdgeInsets.all(8),
+                            child: Stack(
+                              children: <Widget>[
+                                Align(
+                                  alignment: Alignment.topCenter,
+                                  child: Text("Bertin J. Wagueu",
+                                    style: TextStyle(color: Colors.white),),
+                                ),
+                                Align(
+                                  alignment: Alignment.bottomCenter,
+                                  child: Text("Frontend",
+                                    style: TextStyle(color: Colors.white),),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              image: DecorationImage(
+                                fit: BoxFit.fitWidth,
+                                image: ExactAssetImage("assets/about_images/dorline.jpeg"),
+                              ),
+                              color: Colors.indigo,
+                            ),
+                            padding: const EdgeInsets.all(8),
+                            child: Stack(
+                              children: <Widget>[
+                                Align(
+                                  alignment: Alignment.topCenter,
+                                  child: Text("Dorline F. Damesse",
+                                    style: TextStyle(color: Colors.white),),
+                                ),
+                                Align(
+                                  alignment: Alignment.bottomCenter,
+                                  child: Text("Frontend",
+                                    style: TextStyle(color: Colors.white),),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              image: DecorationImage(
+                                fit: BoxFit.fitWidth,
+                                image: ExactAssetImage("assets/about_images/Patricia.jpeg"),
+                              ),
+                              color: Colors.indigo,
+                            ),
+                            padding: const EdgeInsets.all(8),
+                            child: Stack(
+                              children: <Widget>[
+                                Align(
+                                  alignment: Alignment.topCenter,
+                                  child: Text("Therese P. Fotso",
+                                    style: TextStyle(color: Colors.white),),
+                                ),
+                                Align(
+                                  alignment: Alignment.bottomCenter,
+                                  child: Text("Frontend",
+                                    style: TextStyle(color: Colors.white),),
+                                ),
+                              ],
+                            ),
+
+                          ),
+
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        )
+    );
+  }
   );
 }
 
